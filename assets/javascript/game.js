facfbe49af95170185d84da49a880ab6c252b70a
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2019-09-21 20:05:55 -0500

    random word selected

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,8 @@
-var mythicalCreatures = ["dragon", "elf", "kraken", "leprechaun", "mermaid", "phoenix", "sphinx", "troll", "unicorn", "yeti"]
+// var wordList = ["dragon", "elf", "kraken", "leprechaun", "mermaid", "phoenix", "sphinx", "troll", "unicorn", "yeti"]
+var wordList = ["mermaid", "fairy", "elf"];
+// Choose a random Word from the wordList
+var wordChosen = wordList[Math.floor(Math.random()* wordList.length)];
+//HASTA AQUI VAMOS BIEN
 
-var computerGuess = 
+// Show scores (-) for every letter in the wordChosen
+//First you need to determine how many letters has the wordChosen
